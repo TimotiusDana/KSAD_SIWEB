607032f151c5d595b63694a8536b90907276508f
TimotiusDana <221711940@students.uajy.ac.id>
2024-11-13 18:52:46 +0700

    Commit pertama

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Poppins, Lusitana } from 'next/font/google';
+ 
+export const poppins = Poppins({ subsets: ['latin'], weight: ['500', '700'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
